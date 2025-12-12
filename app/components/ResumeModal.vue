37e7cfa74a5d6bd94f1829793dee3a11d163b6a4
jay <wjay.inclino@gmail.com>
2025-12-12 09:41:09 +0800

    update resume to v2

--- app/components/ResumeModal.vue
+++ app/components/ResumeModal.vue
@@ -21,7 +21,7 @@
           <!-- PDF viewer -->
           <div class="flex-1 w-full h-full p-4 md:p-8" @click.stop>
             <iframe 
-              src="/portfolio/my_resume.pdf" 
+              src="/portfolio/my_resume_v2.pdf" 
               class="w-full h-full rounded-lg shadow-2xl"
               type="application/pdf"
             />
